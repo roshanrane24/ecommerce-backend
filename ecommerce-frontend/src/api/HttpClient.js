4dad409699b7501eb2223e7930d5420c8dc7c925
anantgehi <anantgehi@gmail.com>
2022-03-19 10:27:01 +0530

    change in repository structure

--- ecommerce-frontend/src/api/HttpClient.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-const client = axios.create({
-    baseURL: 'http://localhost:8080/api',
-    timeout: 3000
-})
-export default client;
--- ecommerce-frontend/src/components/commons/NotFound.js
+++
@@ -1,7 +0,0 @@
-import Typography from "@mui/material/Typography";
-
-const NotFound = () => {
-    return (<Typography> Page Not Found </Typography>);
-}
-
-export default NotFound;
