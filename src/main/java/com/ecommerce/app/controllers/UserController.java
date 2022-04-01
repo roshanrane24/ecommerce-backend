659c2d9133c1803c4801f6aaf2e15ccefd9f050c
MyGit465 <amangangwar88.4@gmail.com>
2022-04-01 13:19:55 +0530

    Add address

--- src/main/java/com/ecommerce/app/controllers/UserController.java
+++ src/main/java/com/ecommerce/app/controllers/UserController.java
@@ -1,10 +1,19 @@
 package com.ecommerce.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ecommerce.app.dto.request.AddAddressRequest;
+import com.ecommerce.app.dto.response.MessageResponse;
+import com.ecommerce.app.models.Address;
+import com.ecommerce.app.models.AddressType;
+import com.ecommerce.app.models.User;
+import com.ecommerce.app.security.jwt.JwtUtils;
 import com.ecommerce.app.services.IUserService;
 
 @RestController
@@ -12,11 +21,13 @@ import com.ecommerce.app.services.IUserService;
 @CrossOrigin("*")
 public class UserController {
     
+	@Autowired
+	private JwtUtils jwtUtils;
+	
     @Autowired
     IUserService userService;
     
     //Display Profile Details
-    
     
     
     //Display List of Addresses
@@ -24,6 +35,17 @@ public class UserController {
     //Edit Profile Details
     
     //Add Address
+    @PostMapping("/address/add")
+    public ResponseEntity<?> addAddress(@RequestBody AddAddressRequest addAddress){
+    	String email = jwtUtils.getUserNameFromJwtToken(addAddress.getToken());
+		User user = userService.getByEmail(email);
+		Address address = new Address(AddressType.valueOf(addAddress.getTypeOfAddress()), addAddress.getCountry(),addAddress.getState(),addAddress.getFullName(),addAddress.getMobileNumber(),addAddress.getPincode(),addAddress.getLine1(),addAddress.getLine2(),addAddress.getLandmark(),addAddress.getTownCity());
+		System.out.println("Aman");
+		user.getAddresses().put(address.getId(), address);
+		System.out.println("123");
+		userService.saveUser(user);
+		return ResponseEntity.ok(new MessageResponse("Address added successfully ."));
+    }
     
     //Delete Address
     
